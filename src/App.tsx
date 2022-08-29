8c94b4859f4169718a6dded445ae5b1c06b8dce7
서솔민 <solgo123@gmail.com>
2022-08-29 19:00:42 +0900

    :sparkles: google calendar api 심플 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,132 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from '@emotion/styled';
+import { gapi, loadAuth2 } from 'gapi-script';
+import { useEffect, useState } from 'react';
 
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  height: 100vh;
+  align-items: center;
+`;
+
+const event = {
+  summary:
+    '로케이션에 강남역 7번출구 결과값이 있는 데이터 넣었을때 미팅 이메일 발송 시도 결과',
+  location: '강남역 7번출구',
+  description:
+    '디스크립션 디스크립션디스크립션 디스크립션디스크립션 디스크립션디스크립션 디스크립션디스크립션 디스크립션디스크립션 디스크립션디스크립션 디스크립션디스크립션 디스크립션디스크립션 디스크립션디스크립션 디스크립션디스크립션 디스크립션디스크립션 디스크립션디스크립션 디스크립션',
+  start: {
+    dateTime: '2022-08-29T20:23:32',
+    timeZone: 'Asia/Seoul',
+  },
+  end: {
+    dateTime: '2022-08-29T21:21:22',
+    timeZone: 'Asia/Seoul',
+  },
+  attendees: [
+    { email: 'june6723@rubric.im' },
+    { email: 'dogemad93@rubric.im' },
+    { email: 'roroh00903@rubric.im' },
+  ],
+  reminders: {
+    useDefault: false,
+    // overrides: [
+    //   { method: 'email', minutes: 24 * 60 },
+    //   { method: 'popup', minutes: 10 },
+    // ],
+  },
+};
+
+type GoogleBasicProfile = {
+  id: string;
+  name: string;
+  givenName: string;
+  familyName: string;
+  imageUrl: string;
+  email: string;
+};
+const {
+  REACT_APP_CLIENT_ID,
+  REACT_APP_API_KEY,
+  REACT_APP_SCOPES,
+  REACT_APP_DISCOVERY_DOCS,
+} = process.env;
 function App() {
+  const [googleAccountInfo, setGoogleAccountInfo] =
+    useState<GoogleBasicProfile>();
+
+  const setAuth2 = async () => {
+    if (REACT_APP_CLIENT_ID) {
+      await loadAuth2(gapi, REACT_APP_CLIENT_ID, '');
+    }
+  };
+
+  useEffect(() => {
+    setAuth2().then(() => {
+      const user = gapi.auth2
+        .getAuthInstance()
+        .currentUser.get()
+        .getBasicProfile();
+
+      setGoogleAccountInfo({
+        id: user.getId(),
+        name: user.getName(),
+        givenName: user.getGivenName(),
+        familyName: user.getFamilyName(),
+        imageUrl: user.getImageUrl(),
+        email: user.getEmail(),
+      });
+    });
+  }, [gapi]);
+
+  const login = async () => {
+    await gapi.auth2.getAuthInstance().signIn();
+    location.href = '';
+  };
+
+  const logout = async () => {
+    await gapi.auth2.getAuthInstance().signOut();
+    location.href = '';
+  };
+
+  const sendEvent = () => {
+    gapi.load('client', () => {
+      gapi.client.init({
+        apiKey: REACT_APP_API_KEY,
+        clientId: REACT_APP_CLIENT_ID,
+        discoveryDocs: [REACT_APP_DISCOVERY_DOCS ?? ''],
+        scope: REACT_APP_SCOPES,
+      });
+
+      gapi.client.load('calendar', 'v3', () => {
+        gapi.client.calendar.events
+          .insert({
+            calendarId: 'primary',
+            resource: event,
+          })
+          .execute((event: any) => {
+            console.log(event);
+            window.open(event.htmlLink);
+          });
+      });
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrap>
+        {googleAccountInfo ? (
+          <>
+            <p>currentUser: {googleAccountInfo.name}</p>
+            <p onClick={() => sendEvent()}>makeEvent</p>
+            <p onClick={() => logout()}>logout</p>
+          </>
+        ) : (
+          <p onClick={() => login()}>login</p>
+        )}
+      </Wrap>
     </div>
   );
 }
